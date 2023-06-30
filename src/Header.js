5ce16e8bd62ea0c89650c751590b8a6f14d7e64b
spookyboi <ed.zuleta_@live.cl>
2023-06-29 22:36:48 -0400

    primer commit

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,9 @@
 function Header () {
   return (
     <header className='flex p-2 justify-between bg-blue-300 shadow'>
-      <h1 id='titulo'>Titulo</h1>
+      <h1 id='titulo'>SerpiJueguitos</h1>
       <nav className='flex gap-2'>
-        <a href='#'>link</a>
-        <a href='#'>link</a>
-        <a href='#'>link</a>
+
       </nav>
     </header>
   )
