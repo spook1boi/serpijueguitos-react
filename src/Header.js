c512caa00075cd53ef16107d461eda0e09bc8dc3
spookyboi <ed.zuleta_@live.cl>
2023-06-29 22:34:35 -0400

    primer commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+function Header () {
+  return (
+    <header className='flex p-2 justify-between bg-blue-300 shadow'>
+      <h1 id='titulo'>Titulo</h1>
+      <nav className='flex gap-2'>
+        <a href='#'>link</a>
+        <a href='#'>link</a>
+        <a href='#'>link</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
+  
